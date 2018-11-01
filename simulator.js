8b35116f98d920fcd7d7db43cdf97906b13aa7db
Rey <reyneraccel@gmail.com>
2018-11-01 18:37:43 -0300

    Create simulator.js

---
+++ simulator.js
@@ -0,0 +1,11 @@
+export class Simulator {
+    constructor (height, length) {
+        this._height = height;
+        this._length = length;
+        this._cells = {};
+    }
+    simulate () {
+        let modList = [];
+        
+    }
+}
