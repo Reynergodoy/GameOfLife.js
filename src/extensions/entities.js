4065939c74fccca6e418e2969f56e8f73505dd62
Rey <reyneraccel@gmail.com>
2018-11-02 21:27:03 -0300

    Update entities.js

--- src/extensions/entities.js
+++ src/extensions/entities.js
@@ -5,15 +5,16 @@ entities = {
     111
     `)
 }
-function entity(str, xo=0, yo=0){
-    const cleanStr = str.split('\n').join('').split(' ').join('');
-    let arr = [];
+function entity (str, xo = 0, yo = 0){
+    const clean = str.split('\n').join('').split(' ').join('');
+    const cLen = clean.length;
+    const sqrt = Math.sqrt(clean.length);
+    const arr = [];
     let i = 0;
-    const sqrt = Math.sqrt(cleanStr.length);
-    while(i < cleanStr.length) {
+    while (i < cLen) {
         const x = i % sqrt;
         const y = Math.floor(i/sqrt);
-        if(cleanStr[i]==="1")arr.push([x+xo,y+yo]);
+        if (clean[i]==="1") arr.push([x+xo,y+yo]);
         ++i;
     }
     return arr;
