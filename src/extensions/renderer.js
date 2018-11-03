974341fe9e14c7569884fab52a1096bda63f2b9a
Rey <reyneraccel@gmail.com>
2018-11-02 21:24:43 -0300

    Update renderer.js

--- src/extensions/renderer.js
+++ src/extensions/renderer.js
@@ -23,23 +23,23 @@ class GameOfLife extends Simulator{
         this._canvas.width = this._width;
         this._ctx = this._canvas.getContext('2d');
     }
-    start() {
-        this._interval = setInterval(_=>{
+    start () {
+        this._interval = setInterval((_) => {
             this.simulate();
             this.draw();
             this._timestamps.push(new Date().getTime());
             if((this._drawCount % this._speed * 5) === 0){
                 this.outputLog();
             }
-        },1000/this._speed);
+        },1000 / this._speed);
     }
-    outputLog(){
+    outputLog () {
         const now = new Date().getTime();
-        const fps = this._timestamps.filter(time=>time > now - 1000).length;
+        const fps = this._timestamps.filter((time) => time > now - 1000).length;
         const cellCount = JSON.stringify(app._cells).split('rgb').length;
         this._log.innerHTML = `fps: <b>${fps}</b> --- cells: ${cellCount}`;
     }
-    draw() {
+    draw () {
         this._ctx.clearRect(0, 0, this._width, this._height);
         for(let y of Object.keys(this._cells)){
             for(let x of Object.keys(this._cells[y])){
@@ -48,22 +48,22 @@ class GameOfLife extends Simulator{
         }
         ++this._drawCount;
     }
-    drawCell(x, y, color) {
+    drawCell (x, y, color) {
         this._ctx.fillStyle = color;
-        this._ctx.fillRect(x,y,this._cellWidth,this._cellHeight);
+        this._ctx.fillRect(x, y, this._cellWidth, this._cellHeight);
     }
     addObject (obj) {
         const colorArray = Object.keys(obj);
         const colorCount = colorArray.length;
         let i = 0;
-        while(i < colorCount){
+        while (i < colorCount) {
             this.addArray(obj[colorArray[i]], colorArray[i]);
             ++i;
         }
     }
     addArray (arr, colour) {
         let i = arr.length;
-        while(i > 0){
+        while (i > 0) {
             --i;
             this.add(arr[i][1], arr[i][0], colour);
         }
